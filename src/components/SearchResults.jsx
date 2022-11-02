63dc6fd23a16b44a9f15a4e56e55fc0ab6cfb036
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2022-11-02 13:33:02 +0100

    update/backup - before refactoring fetch-method

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,16 +1,46 @@
 import React from 'react'
+import SearchArticleDetails from './SearchArticleDetails'
 
-const SearchResults = (props) => {
+const resultSet = (searchPhrase, articles, totalResults, action) => {
   return (
-    <div className='search-results'>
-        <h2>Relevant Articles: </h2>
-        {props.articles.map(article =>(
-            <div className='result-item' key={article.id}>
+    <div>
+        <h2>Results for: {searchPhrase}</h2> 
+        {articles.map((article, index) =>(
+          <div className='result-item' key={index}>
                 <div className='title'><a href={article.url}>{article.title}</a></div>
                 <div className='description'>{article.description}</div>
             </div>
         ))}
+          {showNextResults(articles, totalResults, action )}
     </div>
+  )
+}
+
+const showNextResults = (articles, totalResults, showNextResults) => {
+  return (
+    <div className="show-next">
+    <p>Showing {articles.length } of {totalResults} results.</p>
+    {articles.length < totalResults && 
+      <button onClick={showNextResults}>Show More Results</button>
+    }
+    </div>
+  )
+}
+
+const emptyResultSet = (searchPhrase)=> {
+  return(
+    <div>
+        <p>Nothing found for {searchPhrase}.</p>
+    </div>
+  )
+}
+
+const SearchResults = (props) => {
+  return (
+    props.requestedSearchPhrase &&
+      <div className='search-results'>
+          {(props.articles.length > 0) ? resultSet(props.requestedSearchPhrase, props.articles, props.totalResults, props.showNextResults) : emptyResultSet(props.requestedSearchPhrase)} 
+      </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools, ReactQueryDevTools} from '@tanstack/react-query-devtools'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client = {queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
